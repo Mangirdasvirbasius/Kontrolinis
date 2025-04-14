670dc3f9e905c7d6c621685ccef895144e60e367
Moksleivis <Moksleivis@kitm.local>
2025-04-14 14:41:56 +0300

    Add existing project files to Git

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ import './App.css'
 function App() {
 
 
+
+
+  
   return (
     <>
-  
+
+
+<form id="forma" action=""></form>
+ Input the first side of the house:     <input type="text" name='x' /> <br></br>
+ Input the hight:    <input type="text" name='y'  /><br></br>
+ Input the second side of the house:    <input type="text" name='z'  /><br />
+<input type="button" onclick="Minecraft()"  value="Submit" />
+
     </>
   )
 }
